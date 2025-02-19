05d5387429d3947385a77528c331f942cea6fc17
andreadg-dev <andydg88@gmail.com>
2025-02-19 10:40:16 +0100

    Add time-based exercises element and funcitonality

--- myScript.js
+++ myScript.js
@@ -139,6 +139,96 @@ async function newCountDown() {
   }, 1000);
 }
 
+/* async function newCountDownSeconds(seconds, htmlElement, digitsColour) {
+  if (!seconds) {
+    alert("Please make sure you select a number!");
+    console.log(`The minutes variable is: ${seconds}`);
+    return;
+  }
+
+  const secondsNum = Number(seconds);
+  if (isNaN(secondsNum) || secondsNum === 0) {
+    alert("Please make sure you select a number greater than 0!");
+    console.log(`The minutes variable is: ${seconds}`);
+    return;
+  }
+
+  if (countdownInterval2) {
+    clearInterval(countdownInterval2);
+    countdownInterval2 = null;
+    if (wakeLock2) {
+      wakeLock2.release().then(() => {
+        wakeLock2 = null;
+      });
+    }
+  }
+
+  try {
+    wakeLock2 = await navigator.wakeLock.request("screen");
+    wakeLock2.addEventListener("release", () => {
+      console.log("Wake Lock was released");
+    });
+    console.log("Wake Lock acquired");
+  } catch (err) {
+    console.error("Error acquiring Wake Lock:", err);
+    // Handle the error (e.g., the browser doesn't support it)
+  }
+
+  countdownInterval2 = setInterval(() => {
+    if (seconds === 0) {
+      // Check *first*
+      clearInterval(countdownInterval2);
+      countdownInterval2 = null;
+      //alert("Countdown Finished!");
+
+      if (wakeLock2) {
+        wakeLock2.release().then(() => {
+          wakeLock2 = null;
+        });
+        console.log("Wake Lock released");
+      }
+      return; // Important: Exit the callback early
+    }
+
+    seconds--;
+
+    htmlElement.text(seconds.toString().padStart(2, "0"));
+    htmlElement.css("color", digitsColour);
+  }, 1000);
+} */
+
+function beep() {
+  const snd = new Audio("./chime-sound.mp3");
+  snd.play();
+}
+
+async function newCountDownSeconds(seconds, htmlElement, digitsColour) {
+  // ... (Your existing validation and wake lock code) ...
+
+  return new Promise((resolve) => {
+    // Return a Promise
+    countdownInterval2 = setInterval(() => {
+      if (seconds === 0) {
+        clearInterval(countdownInterval2);
+        countdownInterval2 = null;
+        if (wakeLock2) {
+          wakeLock2.release().then(() => {
+            wakeLock2 = null;
+          });
+          console.log("Wake Lock released");
+        }
+        resolve(); // Resolve the Promise when countdown finishes
+        return;
+      }
+
+      seconds--;
+      htmlElement.text(seconds.toString());
+      htmlElement.css("color", digitsColour);
+    }, 1000);
+    beep();
+  });
+}
+
 // Initial setup
 slideToggleContent();
 updateSection("#myTenets");
@@ -152,7 +242,6 @@ updateSection("#dropSets", "btn-info", "btn-dark");
 setCopyright();
 
 $("#countdownBtn").on("click", function () {
-  let countdownInterval;
   newCountDown();
 });
 
@@ -175,4 +264,32 @@ $(".controlBtn").on("click", function () {
 
 $(".disclaimer").on("click", function () {
   $(this).toggleClass("expanded");
+});
+
+let countdownInterval2;
+let wakeLock2 = null;
+$("#timeBasedExercisesSeconds").on("click", async function () {
+  let timeBasedExercisesTimer = $("#timeBasedExercisesSeconds");
+  let setTimeSeconds = Number($("#timeForSet").val());
+  let numOfExercises = Number($("#numOfExercises").val());
+  let restTimeSeconds = Number($("#timeForRest").val());
+
+  arrayExercises = [];
+  while (numOfExercises > 0) {
+    arrayExercises.push(setTimeSeconds);
+    if (numOfExercises > 1) {
+      arrayExercises.push(restTimeSeconds);
+    }
+    numOfExercises--;
+  }
+
+  console.log(arrayExercises);
+  for (let index = 0; index < arrayExercises.length; index++) {
+    let color = index % 2 === 0 ? "blue" : "red"; // Simplified color selection
+    await newCountDownSeconds(
+      arrayExercises[index],
+      timeBasedExercisesTimer,
+      color
+    ); // Wait for each countdown
+  }
 });
